5bf61b82cbe910b1293f41c6b603202909ad1b7f
clenildo <clenildo.honorato@accessone.com.br>
2020-03-09 17:53:35 -0300

    Tela de Dashboard iniciada

--- MyFinance/Controllers/TransacaoController.cs
+++ MyFinance/Controllers/TransacaoController.cs
@@ -3,6 +3,8 @@
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using MyFinance.Models;
+    using System;
+    using System.Collections.Generic;
 
     public class TransacaoController : Controller
     {
@@ -74,6 +76,22 @@
 
         public IActionResult Dashboard()
         {
+            List<Dashboard> lista = new Dashboard().RetornarGraficoPie();
+            string valores = "";
+            string labels = "";
+            string cores = "";
+            var random = new Random();
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                valores += lista[i].Total.ToString() + ",";
+                labels += "'" + lista[i].PlanoConta.ToString() + "',";
+                cores += "'" + String.Format("#{0:X6}", random.Next(0x1000000)) + "',";
+            }
+
+            ViewBag.Cores = cores;
+            ViewBag.Labels = labels;
+            ViewBag.Valores = valores;
             return View();
         }
     }
